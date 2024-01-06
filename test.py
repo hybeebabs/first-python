1935cc3a27ec7c427dc694f989e83c60dfd1ca41
Ibrahim Adenekan <hybeeadenekan@gmail.com>
2024-01-06 17:49:05 +0200

    i had shiroh comment

--- test.py
+++ test.py
@@ -41,11 +41,11 @@ with open(working_sheet , 'r') as file:
             output = connection.send_command(each_config)
             print(output)
         connection.disconnect()
-        #saving the output to a new folder
+        #saving the output to a new folder in the  document file
         os.chdir('/home/ibrahim/Documents/script_output/')
         backup_dir = '/home/ibrahim/Documents/script_output/' + working_sheet
         if not os.path.exists(backup_dir):
-            os.makedirs(backup_dir)
+            os.makedirs(backup_dir) i
         fo = open(backup_dir + '/' + router_id, 'w')
         fo.write(output)
         
